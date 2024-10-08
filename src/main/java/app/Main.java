c090859b3e519c426cdcb61f54a7261bc3644ca1
Ømer <cph-oo203@cphbusiness.dk>
2024-10-08 16:05:07 +0200

    initial commit

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,7 +1,12 @@
 package app;
 
+import io.javalin.Javalin;
+import jakarta.persistence.EntityManagerFactory;
+
 public class Main {
     public static void main(String[] args) {
-
+        EntityManagerFactory entityManagerFactory = HibernateConfig.getEntityManagerFactory("pipeline");
+        Javalin app = Javalin.create().start(7000);
+        app.get("/", ctx -> ctx.result("Hello World"));
     }
 }
